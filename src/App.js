9de79fd24447a2954456acf732b2ee45de3140d9
Nabendu <nabendu.biswas@gmail.com>
2020-11-10 22:39:14 +0530

    Part-5 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import MouseContainer from './components/MouseContainer';
+import DataFetching2 from './components/DataFetching2';
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <DataFetching2 />
     </div>
   );
 }
