e8e3b99668a795402bfd0fc8485c893d6d5d5ea7
Nabendu <nabendu.biswas@gmail.com>
2020-11-10 10:37:46 +0530

    Part-3 react hooks complete

---
+++ src/components/HookCounter1.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+const HookCounter1 = () => {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `Clicked ${count} times`
+    })
+
+    return (
+        <button onClick={() => setCount(count+1)}>Click {count} times</button>
+    )
+}
+
+export default HookCounter1
---
+++ src/components/HookCounter2.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+
+const HookCounter2 = () => {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        console.log('useEffect - Updating document title')
+        document.title = `Clicked ${count} times`
+    }, [count])
+
+    return (
+        <>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            <button onClick={() => setCount(count+1)}>Click {count} times</button>
+        </>
+    )
+}
+
+export default HookCounter2
